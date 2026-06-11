c098fcf633bfc95cf4b477040d186bf5df8e290d
mobessir1997 <mobessir1997@gmail.com>
2026-06-11 17:51:22 +0600

    update settings

--- gblog/settings.py
+++ gblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,6 +143,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mobessir1997@gmail.com'
-EMAIL_HOST_PASSWORD = 'ftyznwdbozxsrsxa' 
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS') 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
